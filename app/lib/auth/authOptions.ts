2a32b8474beab0794df657e5365cdef0d32747ea
Sahib-Aujla <sahibpreet568@gmail.com>
2025-09-23 15:31:39 +0530

    added auth

--- app/lib/auth/authOptions.ts
+++ app/lib/auth/authOptions.ts
@@ -1,13 +1,38 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
+import { NextAuthOptions, Session } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { connectMongoDb } from '@/lib/db/mongoose';
 import User from '@/lib/db/models/User.model';
 
+export interface session extends Session {
+    user: {
+        id: string;
+        image: string;
+        email: string;
+        name: string;
+    };
+}
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         })
-    ]
+    ],
+    callbacks: {
+        async session({ session, token }) {
+            const newSession: session = session as session;
+            if (token) {
+                newSession.user.id = token.sub as string;
+            }
+            return session;
+        },
+        async signIn({ user }) {
+            await connectMongoDb();
+            const existingUser = await User.findOne({ email: user.email });
+            if (!existingUser) {
+                await User.create({ userId:user.id, email: user.email, scaAddress: null });
+            }
+            return true;
+        }
+    }
 }
--- app/lib/db/models/User.model.ts
+++ app/lib/db/models/User.model.ts
@@ -1,6 +1,7 @@
 import { Schema, model, models } from 'mongoose';
 
 const userSchema = new Schema({
+    userId: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     scaAddress: { type: String, default: null },
 });
