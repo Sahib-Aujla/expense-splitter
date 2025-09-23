15a0a3b40526478214929638f29ead51f69a6ef2
Sahib-Aujla <sahibpreet568@gmail.com>
2025-09-23 14:52:22 +0530

    changed config

--- app/lib/db/mongoose.ts
+++ app/lib/db/mongoose.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
+
 const connect = async () => {
-  if (mongoose.connection.readyState >= 1) return;
-  await mongoose.connect(process.env.MONGODB_URI!, { dbName: 'expense_splitter' });
-  console.log('Connected to MongoDB');
+    if (mongoose.connection.readyState >= 1) return;
+    await mongoose.connect(process.env.MONGODB_URI!, { dbName: 'expense_splitter' });
+    console.log('Connected to MongoDB');
 };
 export default connect;
