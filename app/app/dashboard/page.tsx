003af465c95cee3d8e6aaed94ac2a320e455314c
Sahib-Aujla <sahibpreet568@gmail.com>
2025-09-25 10:47:46 +0530

    done

--- app/app/dashboard/page.tsx
+++ app/app/dashboard/page.tsx
@@ -1,32 +1,15 @@
 "use client";
-import { constants } from "buffer";
 import { signOut } from "next-auth/react";
-import { useSession } from "next-auth/react";
-import { useEffect } from "react";
+
 export default function Home() {
-  const { data: session, status } = useSession();
-  useEffect(() => {
-    const getAccountData = async () => {
-      if (session && session.user) {
-        const resp = await fetch("/api/wallet/create", { method: "POST" });
-        const d=await resp.json();
-        console.log(d);
-      }
-    };
-    getAccountData();
-  }, [session]);
-  if (status === "loading") {
-    return <div>Loading ....</div>;
-  }
-  console.log(session);
   return (
     <div className="p-4">
-      <h1>DashBoard</h1>
+      <h1>Landing Page </h1>
       <button
         onClick={() => signOut({ redirect: true, callbackUrl: "/" })}
         className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
       >
-        Sign Out
+        Sign in with Google
       </button>
     </div>
   );
