a565a432d689e90c144d901c45cc060cf4a35b0b
Sahib-Aujla <sahibpreet568@gmail.com>
2025-09-25 12:36:48 +0530

    added stuff

--- app/app/Providers.tsx
+++ app/app/Providers.tsx
@@ -1,13 +1,31 @@
-'use client'
+"use client";
 
 import { ReactNode } from "react";
 import { SessionProvider } from "next-auth/react";
+
+import { PrivyProvider } from "@privy-io/react-auth";
+
 type ProvidersProps = {
-    children: ReactNode;
+  children: ReactNode;
 };
 
 const Providers = ({ children }: ProvidersProps) => {
-    return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_APP_ID!}
+      clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
+      config={{
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          ethereum: {
+            createOnLogin: "users-without-wallets",
+          },
+        },
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  );
 };
 
 export default Providers;
